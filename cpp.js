51f4e758cde9f6feddaab9478dbec48fe85548d0
Akhter-Hussain97 <akhterhussaindeveloper@gmail.com>
2024-05-22 16:01:12 +0500

    Add files via upload

--- cpp.js
+++ cpp.js
@@ -1,65 +1,59 @@
-let boxes=document.querySelectorAll('.box')
-let newg=document.querySelector(".new");
-let resetg=document.querySelector(".reset");
-let msgcont=document.querySelector('.msg-container');
-let msg=document.querySelector('#msg');
-const Showwinner=(winner)=>{
-    msg.innerText=`Congratulation Winner are ${winner}` ;
-    msgcont.classList.remove("hide");
-    disableBox();
+let userscore=0;
+let compscore=0;
+const choices=document.querySelectorAll('.choice');
+const msg=document.querySelector('.msg');
+const userscorePara=document.querySelector("#user-score");
+const comp_score=document.querySelector("#comp-score");
+const WinnerGame=(userWin, userchoice, cmpchoice)=>{
+   if(userWin){
+      userscore++;
+      userscorePara.innerText=userscore;
+      console.log("you Win");
+      msg.innerText=`Your ${userchoice} beats ${cmpchoice}`;
+      msg.style.backgoundcolor="green";
+   }
+   else{
+      compscore++;
+      comp_score.innerText=compscore;
+      console.log("you lose");
+      msg.innerText=` Your ${cmpchoice} beats ${userchoice}`;
+      msg.style.backgoundcolor="red";
+   }
 }
-const resetGame=()=>{
-    turnO=true;
-    enableBox();
-    msgcont.classList.add("hide");
-};
-let turnO=true;//player1,player2
-const winnerp=[
-    [0,1,2],
-    [0,3,6],
-    [0,4,8],
-    [1,4,7],
-    [2,5,8],
-    [2,4,6],
-    [3,4,5],
-    [6,7,8],
-]
-const disableBox=()=>{
-    for(let box of boxes){
-        box.disabled=true;
-    }
+const Gencompchoice=()=>{
+   const option=["rock","papers","scissors"];
+   const Rndmno=Math.floor(Math.random()*3);
+   return option[Rndmno];
 }
-const enableBox=()=>{
-    for(let box of boxes){
-        box.disabled=false;
-        box.innerText="";
-    }
+const Draw=()=>{
+   console.log("Draw the game");
+   msg.innerText="Draw the Game";
 }
-boxes.forEach((box)=>{
-    box.addEventListener('click',()=>{
-        if(turnO){
-            box.innerText="O";
-            turnO=false;
-        }
-        else{
-            box.innerText="X";
-            turnO=true;
-        }
-        box.disabled=true;
-        WinnerCheck();
-    })
-});
-const WinnerCheck=()=>{
-    for(let pattern of winnerp){
-        let pos1Val=boxes[pattern[0]].innerText;
-        let pos2Val=boxes[pattern[1]].innerText;
-        let pos3Val=boxes[pattern[2]].innerText;
-        if(pos1Val!=""&& pos2Val!=""&& pos3Val!=""){
-            if(pos1Val=== pos2Val && pos2Val===pos3Val){
-            Showwinner(pos1Val);
-            }
-        }
-    } 
-};
-newg.addEventListener('click',resetGame);
-resetg.addEventListener('click',resetGame);
+const playGame=(userchoice)=>{
+   console.log("user choice=",userchoice);
+   const cmpchoice=Gencompchoice();
+   console.log("comp choice=",cmpchoice);
+   if(userchoice===cmpchoice){
+      Draw();
+   }
+   else{
+      let userWin=true;
+   if(userchoice==="rock"){
+      userWin=cmpchoice==="paper"?false:true;
+   }
+   else if(userchoice==="paper"){
+      userWin=cmpchoice==="scissors"?false:true;
+   }
+   else{
+      userWin=cmpchoice==="rock"?false:true;
+   }
+   WinnerGame(userWin,userchoice,cmpchoice);
+   }
+}
+choices.forEach((choice)=>{
+     choice.addEventListener('click',()=>{
+       const userchoice=choice.getAttribute("id");
+       playGame(userchoice);
+     })
+    });
+ 
